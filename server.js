a0dcb32cd3a5e80fcf394c178a652f28800c361b
Eduardo Odriozola <eduardocorrientes@gmail.com>
2022-08-06 20:37:59 -0300

    limpio codigo, agrego exepcion en MODO

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ import  dotenv from 'dotenv'
 dotenv.config();
 
 import express from 'express';
-import { Server as HttpServer, Server } from 'http';
+import { Server as HttpServer } from 'http';
 import { Server as IOServer } from 'socket.io';
 
 import { engine } from "express-handlebars"
@@ -40,11 +40,11 @@ app.use(express.urlencoded({ extended: true }));
 
 app.use(express.static('public'));
 app.engine(
-    'hbs',
-    engine({
-      extname: '.hbs',
-      defaultLayout: 'main.hbs',
-    })
+	'hbs',
+	engine({
+	  extname: '.hbs',
+	  defaultLayout: 'main.hbs',
+	})
 );
 
 app.set('views', './public/views');
@@ -54,19 +54,19 @@ app.set('view engine', 'hbs');
 app.use(cookieParser());
 
 app.use(
-    session({
-        store: MongoStore.create({
-            mongoUrl:process.env.MONGO_ATLAS_URL,
-            mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true },
-        }),
-        secret: process.env.SESSION_SECRET,
-        resave: true,
-        saveUninitialized: true,
-        rolling: true,
-        cookie: {
-            maxAge: 1000 * 60 * 10,
-        },
-    })
+	session({
+		store: MongoStore.create({
+			mongoUrl:process.env.MONGO_ATLAS_URL,
+			mongoOptions: { useNewUrlParser: true, useUnifiedTopology: true },
+		}),
+		secret: process.env.SESSION_SECRET,
+		resave: true,
+		saveUninitialized: true,
+		rolling: true,
+		cookie: {
+			maxAge: 1000 * 60 * 10,
+		},
+	})
 );
 
 
@@ -80,67 +80,58 @@ app.use('/api/randoms', routerRandomNums);
 
 
 ioServer.on("connection", async (socket) => {
-    console.log("Nuevo usuario conectado");
-    
-    socket.emit("messages", allNormalizeProcess(await mensajes.getAll()));
+	console.log("Nuevo usuario conectado");
+	
+	socket.emit("messages", allNormalizeProcess(await mensajes.getAll()));
   
-    socket.on("new-message", async (msj) => {
-        await mensajes.save(msj);
-        allNormalizeProcess(await mensajes.getAll()),
-        
-            ioServer.sockets.emit("messages",
-                allNormalizeProcess(await mensajes.getAll())
-            );
-    });
+	socket.on("new-message", async (msj) => {
+		await mensajes.save(msj);
+		allNormalizeProcess(await mensajes.getAll()),
+		
+			ioServer.sockets.emit("messages",
+				allNormalizeProcess(await mensajes.getAll())
+			);
+	});
 });
 
 // Ataja los errores de passport
 app.use((error, req, res, next) => {
-    res.status(500).send(error.message);
+	res.status(500).send(error.message);
 });
 
 app.use((req, res) => {
-    res.status(404).send({
-        error: -2,
-        descripcion: `ruta ${req.originalUrl} y metodo ${req.method} no implementada`,
-    });
+	res.status(404).send({
+		error: -2,
+		descripcion: `ruta ${req.originalUrl} y metodo ${req.method} no implementada`,
+	});
 });
 
-// const PORT = yargArgs.puerto || process.env.PORT;
-// const MODO = yargArgs.modo || process.env.MODO;
-const PORT = process.env.PORT || yargArgs.puerto;
-const MODO = process.env.MODO || yargArgs.modo;
+
+const PORT = yargArgs.puerto;
+const MODO = yargArgs.modo;
 const nroCPUs = os.cpus().length;
 
-if(MODO === "CLUSTER" && cluster.isPrimary) {
-    console.log(`Primary PID: ${process.pid} - PORT: ${PORT} - MODO: ${MODO} - isPrimary: ${cluster.isPrimary} - Number of CPUs: ${nroCPUs}\n`);
-    for(let i = 0; i < nroCPUs; i++) {
-        cluster.fork();
-    }
-    cluster.on('online', (worker) => {
-        console.log(`Worker ${worker.process.pid} is alive!`);
-    });
-    cluster.on("exit", (worker) => {
-        console.log(`worker ${worker.process.pid} died`);
-    });
-} else {
-        if (MODO !== 'FORK' && MODO !== 'CLUSTER') {
-            throw new Error(`MODO: ${MODO} no implementado, use "FORK" o "CLUSTER"`);
+
+if (MODO !== 'FORK' && MODO !== 'CLUSTER') {
+    throw new Error(`MODO: ${MODO} no implementado, use "FORK" o "CLUSTER"`);
+};
+
+if (MODO == 'CLUSTER') {
+    if (cluster.isPrimary) {
+        for (let i = 0; i < nroCPUs; i++) {
+            cluster.fork()
         }
-        else if (MODO === "FORK") {
-            console.log(
-                `\nFORK PID: ${process.pid} - PORT: ${PORT} - MODO: ${MODO}\n`
-            );
-        } else if (MODO === "CLUSTER") {
-            console.log(
-                `\nCLUSTER PID: ${process.pid} - PORT: ${PORT} - MODO: ${MODO} - worker: ${cluster.worker.id}`
-            );
-        };
-
-    console.log(`Worker PID: ${process.pid} started`);
-
-    httpServer.listen(PORT, (err) => {
-        if(err) new Error (console.log(err));
-        else console.log(`Servidor corriendo en el puerto: ${PORT} - MODO: ${MODO}`);
-    });
+		console.log(`Primary PID: ${process.pid} - PORT: ${PORT} - MODO: ${MODO} - isPrimary: ${cluster.isPrimary} - Number of CPUs: ${nroCPUs}\n`);
+		cluster.on("online", (worker) => { console.log(`Worker ${worker.process.pid} is alive!`);});
+    	cluster.on("exit", (worker) => { console.log(`Worker ${worker.process.pid} died`);});
+    } else {
+        httpServer.listen(PORT, () => { console.log(`Escuchando en el puerto ${httpServer.address().port} - MODO: ${MODO} - Worker: ${cluster.worker.id} - PID: ${process.pid}`);});
+        httpServer.on("error", (error) => console.error("Error de conexión", error));
+    }
+};
+if (MODO == 'FORK'){
+	httpServer.listen(PORT, () => {
+		console.log(`Escuchando en el puerto ${httpServer.address().port} - MODO: ${MODO} - PID: ${process.pid}`);
+	});
+	httpServer.on("error", (error) => console.error(error, "Error de conexión"));
 };
